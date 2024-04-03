4d75958cb5fc9ba7437973bfba4b04d78e70c9c5
rogeliorodriguez27 <rogelio23rodriguez@gmail.com>
2024-04-03 20:19:23 -0300

    desafio lista de nomes

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,6 +1,6 @@
-import java.util.Scanner;
+// import java.util.Scanner;
 
-public class ContaTerminal {
+/* public class ContaTerminal {
     public static void main(String[] args) throws Exception {
 
         Scanner scanner = new Scanner(System.in);
@@ -23,5 +23,19 @@ public class ContaTerminal {
 //        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %d já está disponível para saque", cliente, agencia, numero, saldo);
         System.out.printf("Olá " +  cliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero +  " e seu saldo " + saldo + " já está disponível para saque");
     
+    } 
+}*/
+
+/**
+ * ContaTerminal
+ */
+public class ContaTerminal {
+
+    public static void main(String[] args) {
+        String contas[] = { "corriente", "ahorros", "salario" };
+        
+        for (String tipo : contas) {
+            System.out.println(tipo);
+        }
     }
 }
---
+++ desafio-pular-nomes/desafio-pular-nomes/src/Problem.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Problem {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+// PRONTO
+		String[] nomes = new String[10];
+		
+		for(int i=0 ; i<10 ; i++)
+			nomes[i] = sc.nextLine();
+			
+			System.out.println(nomes[3]+ "\n"  + nomes[7] + "\n"+ nomes[9] );
+	
+	
+
+}
+}
