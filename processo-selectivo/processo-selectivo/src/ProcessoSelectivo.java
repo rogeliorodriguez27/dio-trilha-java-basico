26af77f46db6191df955368004e655a113cf01ba
rogeliorodriguez27 <rogelio23rodriguez@gmail.com>
2024-04-07 18:34:49 -0300

    Processo-selectivo pronto

--- processo-selectivo/processo-selectivo/src/ProcessoSelectivo.java
+++ processo-selectivo/processo-selectivo/src/ProcessoSelectivo.java
@@ -1,34 +1,93 @@
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSelectivo {
     public static void main(String[] args) throws Exception {
         // analisarCandidato(1000);
         selecaoCandidatos();
+        ArrayList<String> listaCandidatosSelecionados = selecaoCandidatos();
+        imprimirCandidatos(listaCandidatosSelecionados);
+        entrandoEnContato(listaCandidatosSelecionados);
     }
 
-    static void selecaoCandidatos() {
+    static ArrayList<String> selecaoCandidatos() {
         String candidatos[] = { "Pedro", "Lucia", "Jacinta", "José", "Luis", "Pedro", "Pablo" };
-        int candidatosSelecionados = 0, salarioBase = 2000,candidatoAtual = 0;
-        
-        while (candidatosSelecionados <= 5) {
-            if (candidatoAtual >= candidatos.length) {
+        int candidatosSelecionados = 0, salarioBase = 2000, candidatoAtual = 0;
+        ArrayList<String> listaCandidatosSelecionados= new ArrayList<>();
+
+        while (candidatosSelecionados <= 5 && candidatoAtual < candidatos.length) {
+            /*if (candidatoAtual >= candidatos.length) {
                 candidatoAtual--;
-            }
+            }*/
             String candidato = candidatos[candidatoAtual];
             double salarioPretendido = valorPretendido();
             System.out.printf("O candidato %s, solicitou o salario de %.2f\n", candidato, salarioPretendido);
             if (salarioBase >= salarioPretendido) {
-                System.out.printf("O candidato %s foi selecionado para a vaga", candidato);
+                System.out.printf("O candidato %s foi selecionado para a vaga\n", candidato);
                 candidatosSelecionados++;
+                listaCandidatosSelecionados.add(candidato);
+                
 
             }
             candidatoAtual++;
-            
+
+        }
+        return listaCandidatosSelecionados;
+    }
+    
+    static void imprimirCandidatos(ArrayList<String> listaCandidatos) {
+        if (listaCandidatos.size() > 0) {
+            for (int i = 0; i < listaCandidatos.size(); i++) {
+                System.out.printf("Candidato Nº %d: %s\n", i + 1, listaCandidatos.get(i));
+            }
+
+        } else {
+            System.out.println("No hay candidatos seleccionados");
+
         }
     }
 
 
+    static void entrandoEnContato(ArrayList<String> listaCandidatos) {
+        
+     
+        for (String candidato : listaCandidatos) {
+            boolean continuarTentando = true;
+            boolean atendeu = false;
+            int tentativasRealizadas = 1;
 
+        
+            do {
+
+                atendeu = atender();
+                if (atendeu) {
+                    System.out.printf(" o Candidato %s atendeu depois de %d tentativas\n", candidato,
+                            tentativasRealizadas);
+                    continuarTentando = !atendeu;
+                } else 
+                    if (tentativasRealizadas > 2 ) {
+                        System.out.printf(" o Candidato %s não atendeu depois do numero maximo de tentativas: %d\n ",
+                                candidato, tentativasRealizadas);
+                        continuarTentando = false;
+
+                    }
+                
+                tentativasRealizadas++;
+            } while (continuarTentando && tentativasRealizadas <=3 );
+        }
+    }
+    
+
+
+    //metodo auxiliar
+    static boolean atender() {
+        boolean sucesso = new Random().nextInt(3) == 1;
+        if (sucesso) {
+            System.out.println("Contato realizado com sucesso\n");
+        }
+        return sucesso;
+    }
     static void analisarCandidato(double salarioPretendido) {
         double salarioBase = 2000.0;
         if (salarioPretendido < salarioBase) {
